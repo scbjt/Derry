24aea5401454467ecf84436d6902daaca8bd7424
scbjt <116614261+scbjt@users.noreply.github.com>
2022-10-25 11:24:32 +0800

    Add files via upload

---
+++ if选择.c
@@ -0,0 +1,51 @@
+//#include<stdio.h>
+//int main()
+//{
+//	int x,y;
+//	printf("请输入一个整数:");
+///	scanf("%d",&x);
+//	if(x<0)
+//	y=1;
+//	else
+//	if(x==0)
+//	y=0;
+//	else
+//	y=-1;
+//	printf("y=%d\n",y);
+//	return 0;
+	
+//}
+
+#include<stdio.h>
+int main()
+{
+	int x,y;
+	printf("请输入一个整数:");
+	scanf("%d",&x);
+	if(x>=0)
+	if(x>0)
+	y=1;
+	else
+	y=0;
+	else
+	y=-1;
+	printf("y=%d\n",y);
+	return 0;
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
